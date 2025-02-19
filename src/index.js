40da9e178f0eada4d1d9fc886a037923d1ea0e39
Clark Tyra <clarktyra@westat.com>
2025-02-19 14:25:17 -0500

    added dynamic sizing for the map leaflet chrorplethlegend box

--- src/index.js
+++ src/index.js
@@ -413,6 +413,33 @@ $(document).ready(function () {
     });
 });
 
+window.onload = function () {
+    const select = document.querySelector(".leaflet-choroplethlegend-select");
+    const legendgradient = document.querySelector(".leaflet-choroplethlegend-legendgradient");
+
+    console.log('select', select);
+    function adjustWidth() {
+        let temp = document.createElement("span");
+        document.body.appendChild(temp);
+        let maxWidth = 200;
+        let selectedOption = select.options[select.selectedIndex];
+        temp.textContent = selectedOption.text;
+        if (temp.offsetWidth > maxWidth){
+            maxWidth = temp.offsetWidth;
+        }
+        console.log('select maxWidth: ', maxWidth)
+
+        // Cleanup
+        document.body.removeChild(temp);
+       
+        select.style.width = `${maxWidth + 10}px`; // Adding extra padding
+        legendgradient.style.width = `${maxWidth + 10}px`; // Adding extra padding
+    }
+
+    adjustWidth();
+    select.addEventListener("change", adjustWidth); // Adjust on selection change
+};
+
 
 function initUrlParamUpdater () {
     setInterval(() => {
