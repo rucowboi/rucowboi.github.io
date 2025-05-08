f4259a311599344dbe72abdbfabbff779a9aecb2
Clark Tyra <ClarkTyra@westat.com>
2025-05-08 15:49:58 -0400

    fixed the onload select issue

--- src/index.js
+++ src/index.js
@@ -362,26 +362,31 @@ Papa.parsePromise = function (url) {
 };
 
 window.onload = function () {
-    const select = document.querySelector(".leaflet-choroplethlegend-select");
-    const legendgradient = document.querySelector(".leaflet-choroplethlegend-legendgradient");
+    setTimeout(() => {
+        const select = document.querySelector(".leaflet-choroplethlegend-select");
+        if (!select) return;
 
-    function adjustWidth() {
-        let temp = document.createElement("span");
-        document.body.appendChild(temp);
-        let maxWidth = 200;
-        let selectedOption = select.options[select.selectedIndex];
-        temp.textContent = selectedOption.text;
-        if (temp.offsetWidth > maxWidth){
-            maxWidth = temp.offsetWidth;
+        const legendgradient = document.querySelector(".leaflet-choroplethlegend-legendgradient");
+
+        function adjustWidth() {
+            let temp = document.createElement("span");
+            document.body.appendChild(temp);
+            let maxWidth = 200;
+            let selectedOption = select.options[select.selectedIndex];
+            temp.textContent = selectedOption.text;
+            if (temp.offsetWidth > maxWidth){
+                maxWidth = temp.offsetWidth;
+            }
+            document.body.removeChild(temp);
+            select.style.width = `${maxWidth + 10}px`;
+            legendgradient.style.width = `${maxWidth + 10}px`;
         }
-        document.body.removeChild(temp);
-        select.style.width = `${maxWidth + 10}px`;
-        legendgradient.style.width = `${maxWidth + 10}px`;
-    }
 
-    adjustWidth();
-    select.addEventListener("change", adjustWidth);
+        adjustWidth();
+        select.addEventListener("change", adjustWidth);
+    }, 100);
 };
+
 
 
 function initUrlParamUpdater () {
