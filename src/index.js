2f793556fb3b55a79a19d72d09da8ac40ed14964
rucowboi <calchris@cinj.rutgers.edu>
2025-05-23 15:13:04 -0400

    changed default time period to 5 years

--- src/index.js
+++ src/index.js
@@ -68,8 +68,8 @@ var SEARCHOPTIONS_TYPE = [ // filter values for zone or county
 ]
 
 var SEARCHOPTIONS_TIME = [  // filter values for "years" field
-    { value: '01yr', label: "1-Year: 2015"}, // added the 1 year value as initValidateDemographicDataset () was looking for 3 rows #CC25
     { value: '05yrs', label: "5-Year: 2011-2015" },
+    { value: '01yr', label: "1-Year: 2015"}, // added the 1 year value as initValidateDemographicDataset () was looking for 3 rows #CC25
     { value: '10yrs', label: "10-Year: 2006-2015" },
 ];
 
